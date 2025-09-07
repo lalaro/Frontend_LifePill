e4087c3d6a89f31ad73bff9c4e878e7fc9adc40d
Laura Valentina Rodríguez Ortegón <100946295+lalaro@users.noreply.github.com>
2025-09-07 18:26:46 -0500

    ✨Feat (Create OnBoardingScreen): Implementation OnBoardingScreen

--- app/src/main/java/com/escuelaing/edu/lifepill/ui/theme/Theme.kt
+++ app/src/main/java/com/escuelaing/edu/lifepill/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    /*primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80*/
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    /*primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40*/
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -52,7 +52,7 @@ fun LifePillTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        //typography = Typography,
         content = content
     )
 }
